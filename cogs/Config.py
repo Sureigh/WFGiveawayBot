ce72ff46d5ba0e18bb72c9a91ec7b647abd29a6c
Sleigh <63042359+Sureigh@users.noreply.github.com>
2021-04-15 00:26:18 +0900

    Day 1 of ? - Fixed the SQL database
    - Insert seems to work just fine if there are no entries. That's good.
    - The queries seem to work just fine, that's good.
    - The data that comes out of them are a little shaky; That needs to be looked into.
    - I'd say we're about 80% done.

--- cogs/Config.py
+++ cogs/Config.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from discord.ext import commands, tasks
+from discord.ext import commands
 import discord
 import aiosqlite
-from typing import Union, Tuple
+
+DATABASE_NAME = "configs.db"
 
 
 class Config(commands.Cog):
@@ -12,36 +13,50 @@ class Config(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        # Only meant for SELECT queries
-        async def query(*q):
-            async with aiosqlite.connect('configs.db') as db:
-                cursor = await db.execute(*q)
-                return await cursor.fetchone()
-
+        # You feed it the context and tell you what value you want, it gives you the value back. Simple, right?
         async def fetch(ctx, value):
-            return await bot.query("""
-                SELECT ? FROM config
-                WHERE guild = ?
-            """, (value, ctx.guild_id))
+            async with aiosqlite.connect(DATABASE_NAME) as db:
+                # Yes, I know I could use ? instead.
+                # Yes, I tried it.
+                # No, it doesn't work for some reason. Just... let this snippet stay as it is.
+                cursor = await db.execute("""
+                    SELECT {0} FROM config
+                    WHERE guild = {1}
+                """.format(value, ctx.guild_id))
+                result = await cursor.fetchone()
+
+                # If entry is missing/broken/whatever - create a new one from scratch
+                if result is None:
+                    await db.execute("""
+                        INSERT INTO config (guild)
+                        VALUES (?)
+                    """, (ctx.guild_id, ))
+                    await db.commit()
+                    result = await fetch(ctx, value)
+
+            return result
 
         async def hidden(ctx):
             return bool(await fetch(ctx, "hide_command_evocation"))
 
         async def color(ctx):
-            return discord.Color(hex(int(*await fetch(ctx, "embed_colors"), base=16)))
+            return discord.Color(hex(int(await fetch(ctx, "embed_colors"), base=16)))
 
-        bot.query = query
+        async def debug(ctx):
+            return bool(await fetch(ctx, "debug_mode"))
+
         bot.hidden = hidden
         bot.color = color
+        bot.debug = debug
 
         # I swear I'm high as fuck
         async def async_init():
-            async with aiosqlite.connect('configs.db') as db:
+            async with aiosqlite.connect(DATABASE_NAME) as db:
                 await db.execute("""
                     CREATE TABLE IF NOT EXISTS config (
                         guild INTEGER PRIMARY KEY,
                         hide_command_evocation INTEGER DEFAULT 1,
-                        embed_colors TEXT DEFAULT "2c2f33",
+                        embed_colors TEXT DEFAULT '2c2f33',
                         debug_mode INTEGER DEFAULT 0
                     );
                 """)
