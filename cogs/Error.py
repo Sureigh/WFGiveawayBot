a9ccce8c7ef1851a430a72d81036495f1fba8761
Sleigh <63042359+Sureigh@users.noreply.github.com>
2021-04-29 18:35:06 +0900

    Error.py got the fix it needed
    "After 4000 years..."

--- cogs/Error.py
+++ cogs/Error.py
@@ -11,14 +11,15 @@ class Error(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # TODO: Enable only if in Debug mode
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        await ctx.channel.send(f"{error.__class__.__name__}: {error}")
+        if await ctx.bot.debug(ctx):
+            await ctx.channel.send(f"{error.__class__.__name__}: {error}")
 
     @commands.Cog.listener()
     async def on_slash_command_error(self, ctx, error):
-        await ctx.channel.send(f"{error.__class__.__name__}: {error}")
+        if await ctx.bot.debug(ctx):
+            await ctx.channel.send(f"{error.__class__.__name__}: {error}")
 
 def setup(bot):
     bot.add_cog(Error(bot))
